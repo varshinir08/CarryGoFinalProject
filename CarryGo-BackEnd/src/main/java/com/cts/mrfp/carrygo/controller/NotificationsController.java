c4d89077fc85ea2b4bcdded77767a3c67f2a5636
koushik1519 <154420105+KovelaHemaKoushikeswarReddy@users.noreply.github.com>
2026-05-04 23:59:30 +0530

    Update request mapping URL in NotificationsController

--- CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/controller/NotificationsController.java
+++ CarryGo-BackEnd/src/main/java/com/cts/mrfp/carrygo/controller/NotificationsController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("https://carrygo-production.up.railway.app/api/notifications")
+@RequestMapping("/api/notifications")
 @CrossOrigin(origins = "*")
 public class NotificationsController {
 
